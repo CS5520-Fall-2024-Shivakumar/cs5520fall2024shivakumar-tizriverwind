1e39c375c437765a58ffea2e454254cf493b897c
tizriverwind <thisisriverwind@gmail.com>
2024-10-06 01:32:49 -0700

    added resultDisplay and adjusted calDisplay

--- NUMAD24FA_XiaolinLiu/app/src/main/java/com/example/myapplication/QuickCalcActivity.java
+++ NUMAD24FA_XiaolinLiu/app/src/main/java/com/example/myapplication/QuickCalcActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class QuickCalcActivity extends AppCompatActivity {
+public class QuickCalcActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView result, calDisplay;
+    Button button0, button1, button2, button3, button4, button5, button6, button7, button8,
+    button9, buttonPlus, buttonMinus, buttonBack, buttonEquals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,13 @@ public class QuickCalcActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        calDisplay = findViewById(R.id.calcDisplay);
+        result = findViewById(R.id.result);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
